287440951a6c7c1fc38f596c5199b59bf822d50a
Milo <92605623+MiloBlackstar@users.noreply.github.com>
2023-05-23 18:03:56 +0200

    Create site components and link all css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
+import FormSection from './components/FormsSection.jsx'
+import BackgroundSection from './components/BackgroundSection.jsx'
 
 export default function App() {
   return (
     <div className="page">
-
+      <BackgroundSection />
+      <FormSection />
     </div>
   )
 }
---
+++ src/components/BackgroundSection.jsx
@@ -0,0 +1,9 @@
+
+
+export default function BackgroundSection() {
+  return (
+    <header>
+      
+    </header>
+  )
+}
